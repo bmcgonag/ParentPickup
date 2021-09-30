42a2f55fb6fbf7ba3797676c02f6a7f06b4bc734
Brian McGonagill <bmcgonag@gmail.com>
2021-09-30 16:31:11 -0500

    adding systemadmin login

--- client/Accounts/SystemAdmin/systemAdminLogin.js
+++ client/Accounts/SystemAdmin/systemAdminLogin.js
@@ -7,9 +7,38 @@ Template.systemAdminLogin.onRendered(function() {
 });
 
 Template.systemAdminLogin.helpers({
-
+    areFilled: function() {
+        return Session.get("filledFields");
+    },
+    loginError: function() {
+        return Session.get("loginError");
+    },
 });
 
 Template.systemAdminLogin.events({
+    'click #logmein' (event) {
+        event.preventDefault();
+        console.log("clicked login");
+        let email = $("#email").val();
+        let pass = $("#password").val();
 
+        if (email == null || email == "" || pass == "" || pass == null) {
+            Session.set("filledFields", false);
+            return;
+        } else {
+            return Meteor.loginWithPassword(email, pass, function(err, result) {
+                if (err) {
+                    console.log("Error logging in: " + err);
+                    Session.set("loginError", true);
+                } else {
+                    console.log("login result: " + result);
+                    Session.set("loginError", false);
+                }
+            });
+        }
+    },
+    'click #sysAdminReg' (event) {
+        event.preventDefault();
+        FlowRouter.go('/systemAdminReg');
+    },
 });
